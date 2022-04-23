1e8c3cf8b5b636637bbd8ea2e3915487e8d94755
Gokmen Balkan <gokmenbalkan@gmail.com>
2022-04-23 22:32:09 +0300

    ktlint and detekt added

--- app/src/main/java/com/gokmen/musicapp/FirstFragment.kt
+++ app/src/main/java/com/gokmen/musicapp/FirstFragment.kt
@@ -20,13 +20,13 @@ class FirstFragment : Fragment() {
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
